88a032f99cb998a43bd7c4bbc204032a8c0cc374
Yan Zhang <yan.zhang@insureon.com>
2017-10-05 16:22:45 -0500

    add timer for google speech api

--- components/input/speech/speech_to_text/google_speech.py
+++ components/input/speech/speech_to_text/google_speech.py
@@ -2,21 +2,25 @@
 # Requires PyAudio and PySpeech.
 
 import speech_recognition as sr
+from time import clock
 
 def recognize():
+    t0 = clock()
     # Record Audio
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("speech recognition start")
         audio = r.listen(source)
         # print(type(audio))
-
+    t1 = clock()
     # Speech recognition using Google Speech Recognition
     try:
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
         print("You said: " + r.recognize_google(audio))
+        t2 = clock()
+        print('recording: %s seconds, api request: %s seconds' % (t1 - t0, t2 - t1))
         return r.recognize_google(audio)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
